2422b68d94cfa3415164117f9ab8fc4800c34ee0
Kennadee <kennadee.ray@gmail.com>
2021-03-17 00:59:20 -0500

    initial commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace CatWorx.BadgeMaker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double side = 3.14;
+            double area = side * side;
+            Console.WriteLine("area: {0}", area);
+
+        }
+    }
+}
